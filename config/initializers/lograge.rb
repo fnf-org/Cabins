71efc84d52cf7ba6bfc8f745b1c3ecc7567e30bf
casey gerstle <gerstle@gmail.com>
2018-07-30 14:04:27 -0700

    fix user id logging

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -2,9 +2,14 @@ Rails.application.configure do
   config.lograge.enabled = true
 
   config.lograge.custom_payload do |controller|
+    user = if defined? controller.current_user
+             controller.current_user.try(:id)
+           else
+             ''
+           end
     {
         ip: controller.request.ip,
-        user_id: defined? controller.current_user ? '-' : controller.current_user.try(:id)
+        user_id: user
     }
   end
 end
