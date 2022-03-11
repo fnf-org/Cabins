fd14f96078ca1e3aeee39c4e746d1c9f8eb5921b
Mike Matera <matera@lifealgorithmic.com>
2022-03-11 09:51:25 -0800

    In kubernetes source addresses are unreliabble.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.web_console.permissions = '172.18.0.0/16'
+  config.web_console.permissions = '0.0.0.0/0'
 end
