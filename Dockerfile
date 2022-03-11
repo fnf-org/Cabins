c2863f3d7ff7828a96c42fa3ffede06308b0e1e4
Mike Matera <matera@lifealgorithmic.com>
2022-03-11 09:50:30 -0800

    Toward production: Apply migrations on start.

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,5 @@ RUN bundle install
 ADD . /cabins
 
 EXPOSE 3000
-CMD bundle exec rails s -p 3000 -b '0.0.0.0'
+CMD "./entrypoint.sh"
+
---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#! /bin/sh 
+
+set -e 
+
+export DATABASE_URL="mysql2://${CABINS_DB_USER}:${CABINS_DB_PASSWORD}@${CABINS_DB_HOST}:${CABINS_DB_PORT}/${CABINS_DB_NAME}?"
+
+rake db:migrate 
+bundle exec rails s -p 3000 -b '0.0.0.0'
