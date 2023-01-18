c126fdfae07bae63ca9a2697dc8e2b5cd4e6d4ee
Mike Matera <maximus@fatboycentral.com>
2023-01-17 21:28:07 -0800

    Ready to go online.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,24 +2,10 @@
 
 set -e
 
-export DATABASE_URL="mysql2://${CABINS_DB_USER}:${CABINS_DB_PASSWORD}@${CABINS_DB_HOST}:${CABINS_DB_PORT}/${CABINS_DB_NAME}?"
-
-bundle exec rake db:migrate
-
-if ruby db-is-empty.rb; then
-  echo "seeding the DB"
-  cat > db/seed/users.rb << EOF
-User.create(
-  name: '${CABINS_ADMIN_USERNAME}',
-  email: '${CABINS_ADMIN_EMAIL}',
-  password: '${CABINS_ADMIN_PASSWORD}',
-  password_confirmation: '${CABINS_ADMIN_PASSWORD}',
-  phone: '${CABINS_ADMIN_PHONE:=555-1212}',
-  role: '${CABINS_ADMIN_ROLE:=planner}',
-  admin: true
-)
-EOF
-  bundle exec rake db:seed
+# Source the environment from the runtime.
+if [ -f /runtime/config ]; then 
+    . /runtime/config
 fi
 
+# Run the app
 bundle exec rails s -p 3000 -b '0.0.0.0'
